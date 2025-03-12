7d70d9b741e92251bb3b105d6bfcf80c8d998d05
Bobotee3 <pokitran@gmail.com>
2025-03-12 17:43:34 +0000

    submitting

--- battleship/app/src/main/java/bcc/battleship/Grid.java
+++ battleship/app/src/main/java/bcc/battleship/Grid.java
@@ -1,71 +1,106 @@
 package bcc.battleship;
 
 public class Grid {
-    
-    
-    
+    private Location[][] grid;
+
     // Create a new Grid and initialize each Location.
     public Grid() {
-        
+        grid = new Location[Constants.NUM_ROWS][Constants.NUM_COLS];
+        for (int r = 0; r < Constants.NUM_ROWS; r++) {
+            for (int c = 0; c < Constants.NUM_COLS; c++) {
+                grid[r][c] = new Location();
+            }
+        }
     }
-    
+
     // Mark a hit in the specified location.
     public void markHit(int row, int col) {
-       
+        grid[row][col].markHit();
     }
-    
+
     // Mark a miss in the specified location.
     public void markMiss(int row, int col) {
-
+        grid[row][col].markMiss();
     }
-    
+
     // Set the status of the Location at (row, col).
     public void setStatus(int row, int col, int status) {
-
+        grid[row][col].setStatus(status);
     }
-    
+
     // Get the status of the Location at (row, col).
     public int getStatus(int row, int col) {
-        return 0;
+        return grid[row][col].getStatus();
     }
-    
+
     // Return whether this Location has already been guessed.
     public boolean alreadyGuessed(int row, int col) {
-        return false;
+        return !grid[row][col].isUnguessed();
     }
-    
+
     // Set whether there is a ship at this location.
     public void setShip(int row, int col, boolean val) {
-
+        grid[row][col].setShip(val);
     }
-    
+
     // Return whether there is a ship at this location.
     public boolean hasShip(int row, int col) {
-        return false;
+        return grid[row][col].hasShip();
     }
-    
+
     // Get the Location object at this row and column.
     public Location get(int row, int col) {
-        return null;
+        return grid[row][col];
     }
-    
+
     // Return the number of rows.
     public int numRows() {
-        return 0;
+        return Constants.NUM_ROWS;
     }
-    
+
     // Return the number of columns.
     public int numCols() {
-        return 0;
+        return Constants.NUM_COLS;
     }
-    
 
-    //maybe convert to boolean?
+    // Add a ship to the grid if valid, otherwise return false.
     public boolean addShip(Ship s) {
-        return false;
+        int row = s.getRow();
+        int col = s.getCol();
+        int length = s.getLength();
+        int direction = s.getDirection();
+
+        if (direction == Constants.UNSET) return false;
+
+        if (direction == Constants.HORIZONTAL) {
+            if (col + length > Constants.NUM_COLS) return false;
+            for (int i = 0; i < length; i++) {
+                if (hasShip(row, col + i)) return false;
+            }
+            for (int i = 0; i < length; i++) {
+                setShip(row, col + i, true);
+            }
+        } else {
+            if (row + length > Constants.NUM_ROWS) return false;
+            for (int i = 0; i < length; i++) {
+                if (hasShip(row + i, col)) return false;
+            }
+            for (int i = 0; i < length; i++) {
+                setShip(row + i, col, true);
+            }
+        }
+        return true;
     }
 
-    public boolean allShipsSank(){
-        return false;
+    // Check if all ships have been sunk.
+    public boolean allShipsSank() {
+        for (int r = 0; r < Constants.NUM_ROWS; r++) {
+            for (int c = 0; c < Constants.NUM_COLS; c++) {
+                if (grid[r][c].hasShip() && !grid[r][c].checkHit()) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
--- battleship/app/src/main/java/bcc/battleship/Player.java
+++ battleship/app/src/main/java/bcc/battleship/Player.java
@@ -1,24 +1,32 @@
 package bcc.battleship;
-import bcc.battleship.Constants;
 
 public class Player {
-  
+    private Ship[] ships;
+    private Grid myGrid;
+    private Grid opponentGrid;
+
     // Constructor: Initialize the grids and the ships.
     public Player() {
-
+        myGrid = new Grid();
+        opponentGrid = new Grid();
+        ships = new Ship[Constants.SHIP_LENGTHS.length];
+        for (int i = 0; i < ships.length; i++) {
+            ships[i] = new Ship(Constants.SHIP_LENGTHS[i]);
+        }
     }
-    
+
     /**
      * This method is used for testing to set a ship's location.
      * It sets the ship's row, column, and direction, then adds it to the player's grid.
      *
      */
-    
     public boolean chooseShipLocation(int index, int row, int col, int direction) {
-        
-        return false;
+        if (index < 0 || index >= ships.length) return false;
+        ships[index].setLocation(row, col);
+        ships[index].setDirection(direction);
+        return myGrid.addShip(ships[index]);
     }
-   
+
     /**
      * Record a guess from the opponent.
      * This method checks the player's grid at (row, col). If there is a ship,
@@ -26,15 +34,20 @@ public class Player {
      *
      */
     public boolean recordOpponentGuess(int row, int col) {
-        return false;
+        if (myGrid.hasShip(row, col)) {
+            myGrid.markHit(row, col);
+            return true;
+        } else {
+            myGrid.markMiss(row, col);
+            return false;
+        }
     }
-    
-    
+
     public Grid getMyGrid() {
-        return null;
+        return myGrid;
     }
-    
+
     public Grid getOpponentGrid() {
-        return null;
+        return opponentGrid;
     }
 }
